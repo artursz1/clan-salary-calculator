ded405811bab403e854c4d88fd664e3b25365a10
artursz1 <arturszabo7@gmail.com>
2023-01-31 10:24:49 +0200

    second commit

--- script.js
+++ script.js
@@ -3,10 +3,7 @@ let hex1 = "000000";
 let hex2 = "000000";
 
 document.querySelector("#generate").addEventListener("click", () => {
-    generateRandomHex()
-});
-document.querySelector("#copy").addEventListener("click", () => {
-    copyHex();
+    generateRandomHex();
 });
 
 function generateRandomHex() {
@@ -17,15 +14,6 @@ function generateRandomHex() {
         hex1 += hexNumbers[Math.floor(Math.random() * hexNumbers.length)];
         hex2 += hexNumbers[Math.floor(Math.random() * hexNumbers.length)];
     }
-
-    document.querySelector("#hex1").innerHTML = hex1;
-    document.querySelector("#hex2").innerHTML = hex2;
     
     document.body.style.background = `linear-gradient(45deg, #${hex1}, #${hex2})`;
-}
-
-function copyHex() {
-    const gradient = `background: linear-gradient(45deg, #${hex1}, #${hex2});`;
-    
-    navigator.clipboard.writeText(gradient);
 }
